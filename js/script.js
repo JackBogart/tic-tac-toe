516a3fce767f6cda179b71bbdfb873f9d570c777
Jack Bogart <jackbogart0822@gmail.com>
2024-05-26 19:16:12 -0400

    Add the game controller object
    This object controls the game state such as checking for a winner and
    exposes selectCell so the eventual DOM controller will be able to
    interact with a simple API.

--- js/script.js
+++ js/script.js
@@ -10,3 +10,50 @@ function createPlayer(marker) {
     const getMarker = () => marker;
     return { getMarker };
 }
+
+const gameController = (function () {
+    const player1 = createPlayer('X');
+    const player2 = createPlayer('O');
+    let isOver = false;
+    let currentPlayer = player1;
+
+    const winStates = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+
+    const getCurrentPlayer = () => currentPlayer.getMarker();
+
+    const selectCell = (index) => {
+        if (isOver) return; // Current game has finished
+        if (gameBoard.getBoard()[index] != '') return;
+        gameBoard.setMarker(index, currentPlayer.getMarker());
+
+        if (checkForWin()) {
+            isOver = true;
+            console.log(`${currentPlayer.getMarker()} has won`);
+        } else {
+            currentPlayer = currentPlayer === player1 ? player2 : player1;
+        }
+    };
+
+    const checkForWin = () => {
+        board = gameBoard.getBoard();
+        currentMarker = currentPlayer.getMarker();
+
+        for (const winState of winStates) {
+            if (winState.every((index) => board[index] === currentMarker)) {
+                return true;
+            }
+        }
+        return false;
+    };
+
+    return { selectCell, getCurrentPlayer };
+})();
